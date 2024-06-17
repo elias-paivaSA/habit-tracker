f9d9bd8a6d63d9af1e9c66da4ecd3b91aefef031
Elias <eliaspsa13@gmail.com>
2024-06-17 11:19:11 -0300

    updating the project

--- javascript/script.js
+++ javascript/script.js
@@ -4,21 +4,11 @@ const btnLogin = document.getElementById("btn-login");
 const fieldReq1 = document.getElementById("fieldReq1");
 const fieldReq2 = document.getElementById("fieldReq2");
 
-const arrayUsername = [];
-const arrayOneItem = [];
-const passwordArray = [];
-const lastPassword = [];
-
 userName.addEventListener("keyup", () => {
-    arrayUsername.push(userName.value)
-    const lastItemArray = arrayUsername.slice(-1);
-    arrayOneItem.push(lastItemArray);
     const passwordLength = password.value.length;
     const userNameLength = userName.value.length;
-    arrayOneItem.forEach((element) => {
-        const numberOfCharachters = element.join("").length;
-        if(numberOfCharachters > 0) fieldReq1.style.display = "block";
-        if(numberOfCharachters < 1) {
+        if(userNameLength) fieldReq1.style.display = "block";
+        if(!userNameLength) {
             fieldReq1.style.display = "none";
             btnLogin.setAttribute("disabled", true);
         }
@@ -27,7 +17,6 @@ userName.addEventListener("keyup", () => {
         btnLogin.removeAttribute("disabled");
     }
     })
-})
 
 password.addEventListener("keyup", () => {
     const passwordLength = password.value.length;
@@ -36,13 +25,13 @@ password.addEventListener("keyup", () => {
         fieldReq2.style.display = "block";
         btnLogin.removeAttribute("disabled");
     }
-    if(passwordLength < 1) {
+    if(!passwordLength) {
         fieldReq2.style.display = "none";
         btnLogin.setAttribute("disabled", true);
     }
-})
 
-btnLogin.addEventListener("click", () => {
-
+    if(!userNameLength && passwordLength) {
+        fieldReq2.style.display = "block";
+    }
 })
 
