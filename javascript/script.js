39d2ca8e4b7373e39a442380e5ccdb0f7e9f0529
Elias <eliaspsa13@gmail.com>
2024-06-17 08:41:31 -0300

    updating the project

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,7 @@
-//const body = document.getElementById("body");
-//console.log(body);
 const userName = document.getElementById("uname");
 const password = document.getElementById("pass");
 const btnLogin = document.getElementById("btn-login");
 const fieldReq1 = document.getElementById("fieldReq1");
-const fieldReq2 = document.getElementById("fieldReq2");
 
 const arrayUsername = [];
 const arrayOneItem = [];
@@ -12,20 +9,16 @@ const arrayOneItem = [];
 userName.addEventListener("keyup", () => {
     arrayUsername.push(userName.value)
     const lastItemArray = arrayUsername.slice(-1);
-    const lastItemArrayToArray = arrayOneItem.push(lastItemArray);
+    arrayOneItem.push(lastItemArray);
     arrayOneItem.forEach((element) => {
         const numberOfCharachters = element.join("").length;
-        console.log(numberOfCharachters);
-        if(numberOfCharachters > 1) {
-            console.log(btnLogin);
+        if(numberOfCharachters > 0) {
             btnLogin.removeAttribute('disabled');
             fieldReq1.style.display = "block";
-            fieldReq2.style.display = "block";  
         }
         if(numberOfCharachters < 1) {
             btnLogin.removeAttribute('disabled');
             fieldReq1.style.display = "none";
-            fieldReq2.style.display = "none";  
         }
     })
 })
