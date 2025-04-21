d3c0dfc9de1acc248be1b8e075101c7dfe47ecc8
Elias Paiva <eliaspsa13@gmail.com>
2025-04-21 15:49:15 -0300

    setting up authentication

--- javascript/login.js
+++ javascript/login.js
@@ -1,2 +1,25 @@
 const userInput = document.getElementById("user");
-console.log(userInput);
+const password = document.getElementById("password");
+const button = document.getElementById("button");
+
+if (button.disabled) {
+    button.classList.remove("hover:bg-blue-700");
+    button.classList.add( 'cursor-not-allowed' );
+    button.classList.add( 'opacity-50');   
+}
+
+userInput.addEventListener("keyup", (e) => {
+    e.preventDefault();
+    const inputLength = userInput.value.length;
+    const passwordLength = password.value.length;
+    if (inputLength >= 5 && inputLength <= 10) {
+        passwordLength.addEventListener("keyup", (e) => {
+            if (passwordLength >= 5 && passwordLength <= 10) {
+                button.classList.add("hover:bg-blue-700");
+                button.classList.remove('cursor-not-allowed');
+                button.classList.remove('opacity-50');
+            }
+        })
+    };
+  });
+
